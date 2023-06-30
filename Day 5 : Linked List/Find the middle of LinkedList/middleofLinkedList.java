eeabec7eb55c3e2ca61264ae44df4ede11944666
kastrahl <56175823+kastrahl@users.noreply.github.com>
2023-06-30 07:01:15 +0530

    Update middleofLinkedList.java

--- Day 5 : Linked List/Find the middle of LinkedList/middleofLinkedList.java
+++ Day 5 : Linked List/Find the middle of LinkedList/middleofLinkedList.java
@@ -1,1 +1,40 @@
+import org.w3c.dom.Node;
+/****************************************************************
 
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node middleNode(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
